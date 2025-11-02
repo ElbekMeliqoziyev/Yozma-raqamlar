5087e93693b1fcfff050fa03aa66fcf398733e35
Elbek <meliqoziyevelbek7@gmail.com>
2025-11-02 16:42:54 +0500

    fist step

---
+++ num.py
@@ -0,0 +1,38 @@
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+from deep_translator import GoogleTranslator
+from num2words import num2words
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+
+
+TOKEN = os.getenv("TOKEN")
+
+db = Dispatcher()
+
+@db.message(CommandStart())
+async def starter(message: Message):
+    await message.answer(f"Asslomu alaykum {message.from_user.first_name}, Botga xush kelibsiz \nIxtiyoriy raqam yozsangiz men sizga yozma ko'rinishini yuboraman")
+
+@db.message()
+async def tarjima(message: Message):
+    try:
+        a = num2words(int(message.text))
+        turn = GoogleTranslator(source='auto', target='uz').translate(a)
+        await message.answer(turn)
+    except:
+        await message.answer("Iltimos raqam yozing")
+
+
+async def boshla():
+    print("bot ishda")
+    bot = Bot(TOKEN)
+    await db.start_polling(bot)
+
+asyncio.run(boshla())
+
+
